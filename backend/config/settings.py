bb16323ffaeb193c8303ac96c7ee7172dce95714
Faso-main <faso.312@yandex.ru>
2025-05-05 09:36:14 +0400

    update pathing | 5_5_25 9:35

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -2,8 +2,8 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    T5_MODEL_PATH: str = os.getenv('T5_MODEL_PATH', 'models/fake_t5-small_20ep')
-    T5_LABEL_PATH: str = os.getenv('T5_LABEL_PATH', 'models/label2id.json')
+    T5_MODEL_PATH: str = os.getenv('T5_MODEL_PATH', 'backend/models/fake_t5-small_20ep')
+    T5_LABEL_PATH: str = os.getenv('T5_LABEL_PATH', 'backend/models/label2id.json')
     MAX_LEN: int = int(os.getenv('MAX_LEN', 128))
     DEVICE: str = os.getenv('DEVICE', 'cpu')
 
